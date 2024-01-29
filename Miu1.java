2a681d707d888891a0fd1060705349cc038e999e
kasongo-mavetya-emmanuel <emmanuelkasongomavetya@gmail.com>
2024-01-29 09:53:00 +0000

    find Poe tested

--- Miu1.java
+++ Miu1.java
@@ -3,6 +3,9 @@
  */
 
 package com.mycompany.miu1;
+import java.util.Arrays;
+
+
 
 /**
  *
@@ -66,33 +69,92 @@ public class Miu1 {
         return reversed;
         
     }
+    
+    //question 5
+    static int[] commonElts(int[]arr1,int[]arr2){
+        int len=(arr1.length<arr2.length)?arr1.length:arr2.length;
+
+        int[] result= new int[len];
+        int k=0;
+        for(int i=0;i<arr1.length;i++){
+            for(int j=0;j<arr2.length;j++){
+                if(arr1[i]==arr2[j]){
+                    result[k]=arr1[i];
+                    k++;
+                }
+            }
+            
+        }
+        int[] arr=new int[k];
+        
+        for(int i=0;i<arr.length;i++){
+            arr[i]=result[i];
+        }
+        
+        return arr;
+        
+            
+    }
+    
+    static int findPOE(int[] arr){
+        int totalSum=0;
+        int n= arr.length;
+        int leftSum=0;
+        
+        if(n<3) return -1;
+        
+        for(int num : arr){
+            totalSum+=num;
+        }
+        for(int i=0;i<n;i++){
+            totalSum-=arr[i];
+            
+            if(leftSum==totalSum){
+                return i;
+            }
+            
+            leftSum+=arr[i];
+        }
+        return -1;
+    }
+
 
     public static void main(String[] args) {
-        //question 1
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {3, 2, 1, 4, 5};
-        int[] arr3 = {1, 2, 3, 4, 5, 6};
-
-        isCenteredArray(arr1);
-        isCenteredArray(arr2);
-        isCenteredArray(arr3);
-        System.out.println(isCenteredArray(arr1));
-        System.out.println(isCenteredArray(arr2));
-        System.out.println(isCenteredArray(arr3));
+//        //question 1
+//        int[] arr1 = {1, 2, 3, 4, 5};
+//        int[] arr2 = {3, 2, 1, 4, 5};
+//        int[] arr3 = {1, 2, 3, 4, 5, 6};
+//
+//        isCenteredArray(arr1);
+//        isCenteredArray(arr2);
+//        isCenteredArray(arr3);
+//        System.out.println(isCenteredArray(arr1));
+//        System.out.println(isCenteredArray(arr2));
+//        System.out.println(isCenteredArray(arr3));
+//        
+//        
+//        //question 2
+//        System.out.println(sumOddEven(arr1));
+//        
+//        //question 3
+//         char[] inputArray = {'a', 'b', 'c', 'd', 'e', 'f'};
+//        int startPosition = 2;
+//        int length = 3;
+//        
+//        System.out.println(charArr(inputArray, startPosition, length));
+//        
+//        //question 4
+//        System.out.println(reverseNumber(134));
         
-        
-        //question 2
-        System.out.println(sumOddEven(arr1));
-        
-        //question 3
-         char[] inputArray = {'a', 'b', 'c', 'd', 'e', 'f'};
-        int startPosition = 2;
-        int length = 3;
-        
-        System.out.println(charArr(inputArray, startPosition, length));
-        
-        //question 4
-        System.out.println(reverseNumber(134));
+        //question 5
+//         int[]arr1={1, 8, 3, 2,6};
+//         int[] arr2={ 2, 6, 1};
+//         int[] result= commonElts(arr1,arr2);
+//        
+//        System.out.println(Arrays.toString(result));
+         //question 6
+         int[] findPoeArr={1, 8, 3, 7, 10, 2};
+         System.out.println(findPOE(findPoeArr));
 
 
 
